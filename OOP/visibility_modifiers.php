288e4683ded8da366a930560b0b0d39934d6e15d
usr <per@example.com>
2018-12-08 14:29:57 +0100

    visibility modifiers example

--- OOP/visibility_modifiers.php
+++ OOP/visibility_modifiers.php
@@ -4,6 +4,27 @@
 
 <?php
 
+class User {
+  private $is_admin = false;
+
+  private function has_admin_access(){
+    return $this->is_admin === true;
+  } // has_admin_access();
+
+  public function access_level(){
+    return $this->has_admin_access() ? 'Admin' : 'Standard';
+  } // access_level();
+
+
+} // User
+
+
+$user = new User;
+echo $user->is_admin; // error: cant access private property
+$user->is_admin = true; // error: cant access private property
+echo $user->has_admin_access(); // error: call to private method
+echo $user->access_level(); // 'Standard';
+
 
 
  ?>
